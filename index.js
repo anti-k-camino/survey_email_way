d74d6fa9553769327df087f074face8d5972f6e3
antiq <kratyukan@gmail.com>
2020-06-16 14:05:50 +0300

    initial

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send({ msg: 'Hello' }));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Running on ${port}...`));
