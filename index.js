b2d17aee2c6b771f3d75d51be080de73b8b109f9
antiq <kratyukan@gmail.com>
2020-06-18 11:37:49 +0300

    google+ config

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => res.send({ msg: 'Hello' }));
+// 92672626140-u0t1k2nkvm6l24q5qnppthto3t27q524.apps.googleusercontent.com
+
+// VatzstSNOeVJ7WwkqnttPnXa
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: 'http://www.example.com/auth/google/callback'
+  }, (accessToken, refreshToken, profile, cb) => {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }));
+
+app.get('/', (req, res) => {
+  res.send({ msg: 'Hello' }));
+};
 
 const port = process.env.PORT || 3000;
 
